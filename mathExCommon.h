5e86b13bb20f99f596981463b3bad3554f614080
Pyotr-Ivanov <write-pyotr@proton.me>
2025-06-24 15:34:06 +0200

    added struct folder

---
+++ mathExCommon.h
@@ -0,0 +1,12 @@
+
+
+
+#ifndef MATHEX_COMMON
+#define MATHEX_COMMON 1
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <errno.h>
+
+#endif // !MATEX_COMMON
---
+++ structs/tree.h
@@ -0,0 +1,17 @@
+
+
+
+#ifndef MATHEX_COMMON
+#define MATHEX_COMMON 1
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <errno.h>
+
+#endif // !MATEX_COMMON
+
+#ifndef MATHEX_TREE
+#define MATHEX_TREE 1
+
+#endif // !MATHEX_TREE
