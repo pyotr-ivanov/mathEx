8df77aef54d7ee66e4e8c01f2cf28ecd28c2c0fc
Pyotr-Ivanov <write-pyotr@proton.me>
2025-07-12 16:11:42 +0200

    added basic operator parsing and depth logic

--- mathExParser.c
+++ mathExParser.c
@@ -14,23 +14,79 @@ mathExAgent_t* mathExInit() {
     }
 
     new->clean = true;
-    new->opFlag = true;
+    new->opFlag = false;
 
     return new;
 }
 
 
+void mathExAddNode(mathExAgent_t* parser, uint8_t type, uint8_t operation, double data) {
+    if (parser == NULL) {
+        return;
+    }
+
+    parser->pos = mathExTreeInsert(parser->pos, type, operation, data, parser->depth);
+
+    if (parser->tree == NULL || (parser->pos->depth < parser->tree->depth)) {
+        parser->tree = parser->pos;
+    }
+
+    return;
+}
+
 
 ptrStdTreeNode_t* mathExBuildTree(mathExAgent_t* parser, char* expression, size_t expLen) {
     if (parser == NULL) {
         return NULL;
-    } else if (parser->clean == false) {
+    } else if (parser->clean == false || expression == NULL) {
         return parser;
     }
     parser->expression = expression;
     parser->expLen = expLen;
 
+    size_t i = 0;
+    while (*(parser->expression + i) != '\0' && i < parser->expLen) {   
 
+        if (*(parser->expression + i) == '(') {
+            if (parser->opFlag == false && parser->clean == false) {            // add multiplication
+                mathExAddNode(parser, MATHEX_NODET_OP, MATHEX_OPT_MUL, 0.0);
+            }
+            parser->depth = parser->depth + 1;
+
+        } else if (*(parser->expression + i) == ')') {
+            parser->depth = parser->depth - 1;
+
+        } else if (*(parser->expression + i) == '+') {
+            mathExAddNode(parser, MATHEX_NODET_OP, MATHEX_OPT_ADD, 0.0);
+
+        } else if (*(parser->expression + i) == '-') {
+            mathExAddNode(parser, MATHEX_NODET_OP, MATHEX_OPT_SUB, 0.0);
+
+        } else if (*(parser->expression + 1) == '*') {
+            if (*(parser->expression + i + 1) == '*') {
+                i++;
+                mathExAddNode(parser, MATHEX_NODET_OP, MATHEX_OPT_POT, 0.0);
+            } else {
+                mathExAddNode(parser, MATHEX_NODET_OP, MATHEX_OPT_MUL, 0.0);
+            }
+
+        } else if (*(parser->expression + i) == '/') {
+            mathExAddNode(parser, MATHEX_NODET_OP, MATHEX_OPT_DIV);
+
+        } else if (isalpha(parser->expression + i) != 0) {
+            // TODO:: Add logic for variables
+
+        } else if (isdigit(parser->expression + i) != 0) {
+            // TODO:: Add logic for parsing values
+
+        } else {
+            i++;
+            continue;
+        }
+
+        parser->clean = false;
+        i++;
+    }
 }
 
 
@@ -45,7 +101,7 @@ mathExAgent_t* mathExResetAgent(mathExAgent_t* parser) {
     memset(parser, '\0', sizeof(mathExAgent_t));
 
     parser->clean = true;
-    parser->opFlag = true;
+    parser->opFlag = false;
     return parser;
 }
 
